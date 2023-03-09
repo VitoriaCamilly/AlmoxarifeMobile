06211f8dfffea2af7b87698ea61772924e41a0ec
viniciusz1 <vinicius101313@gmail.com>
2023-03-08 21:03:08 -0300

    TENTANDO FAZER RECICLERVIEW

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -5,20 +5,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentHomeBinding;
+import com.example.myapplication.ui.model.Classificacao;
+import com.example.myapplication.ui.model.Produto;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private HomeAdapter homeAdapter;
+    private List<Produto> listaProdutos;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,6 +43,16 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
+//        listaProdutos.add(new Produto(1, 1, "Primeiro Produto", new Classificacao(1, "primeira Classificacao")));
+//        listaProdutos.add(new Produto(1, 1, "Primeiro Produto", new Classificacao(1, "primeira Classificacao")));
+//        listaProdutos.add(new Produto(1, 1, "Primeiro Produto", new Classificacao(1, "primeira Classificacao")));
+//        listaProdutos.add(new Produto(1, 1, "Primeiro Produto", new Classificacao(1, "primeira Classificacao")));
+//        listaProdutos.add(new Produto(1, 1, "Primeiro Produto", new Classificacao(1, "primeira Classificacao")));
+        listaProdutos.add(new Produto(1, 1, "Primeiro Produto", new Classificacao(1, "primeira Classificacao")));
+
+        homeAdapter = new HomeAdapter(getContext(), listaProdutos, this);
         return root;
     }
 
--- app/src/main/java/com/example/myapplication/ui/model/Produto.java
+++ app/src/main/java/com/example/myapplication/ui/model/Produto.java
@@ -3,6 +3,7 @@ package com.example.myapplication.ui.model;
 import java.util.List;
 
 public class Produto {
+
     private Integer codigo;
     private Integer quantidadeTotal;
     private Integer quantidadeReservada;
@@ -12,4 +13,86 @@ public class Produto {
     private String imagem;
     private List<Localizacao> localizacoes;
     private Classificacao classificacao;
+
+    public Produto(int codigo,int quantidadeTotal,String nome, Classificacao classificacacao){
+        this.codigo = codigo;
+        this.quantidadeTotal = quantidadeTotal;
+        this.nome = nome;
+        this.classificacao = classificacacao;
+    }
+
+
+
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getQuantidadeTotal() {
+        return quantidadeTotal;
+    }
+
+    public void setQuantidadeTotal(Integer quantidadeTotal) {
+        this.quantidadeTotal = quantidadeTotal;
+    }
+
+    public Integer getQuantidadeReservada() {
+        return quantidadeReservada;
+    }
+
+    public void setQuantidadeReservada(Integer quantidadeReservada) {
+        this.quantidadeReservada = quantidadeReservada;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public OpcaoUso getOpcaoUso() {
+        return opcaoUso;
+    }
+
+    public void setOpcaoUso(OpcaoUso opcaoUso) {
+        this.opcaoUso = opcaoUso;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+    public List<Localizacao> getLocalizacoes() {
+        return localizacoes;
+    }
+
+    public void setLocalizacoes(List<Localizacao> localizacoes) {
+        this.localizacoes = localizacoes;
+    }
+
+    public Classificacao getClassificacao() {
+        return classificacao;
+    }
+
+    public void setClassificacao(Classificacao classificacao) {
+        this.classificacao = classificacao;
+    }
 }
